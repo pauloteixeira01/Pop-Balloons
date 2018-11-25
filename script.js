e8d20bcd015077a2405e05fe7f8ecb7b98130fb0
PauloTeixeira <paulo.teixeira1985@hotmail.com>
2018-11-25 18:58:41 -0300

    JogoDeEstourarBaloes

---
+++ script.js
@@ -0,0 +1,23 @@
+function addbola(){
+	var bola = document.createElement("div");
+	bola.setAttribute("class", "bola");
+
+	var p1 = Math.floor(Math.random() * 1200);
+	var p2 = Math.floor(Math.random() * 500);
+
+	// Cor aleatória das bolas
+	var cor = parseInt(Math.random() * 999999);
+	bola.setAttribute("style", "left:"+p1+"px;top:"+p2+"px;background-color:#"+cor);
+		
+	bola.setAttribute("onclick","estourar(this)");
+
+	document.body.appendChild(bola);
+}
+// Função que estoura os elementos
+function estourar(elemento){
+	document.body.removeChild(elemento);
+}
+// Função que da início a tudo
+function iniciar(){
+	setInterval(addbola, 500);
+}
